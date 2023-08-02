dd3e57c39d06a5ab431f647552dce0fb85f14ce3
Md Enam <aws.enam@gmail.com>
2023-08-02 14:32:07 +0530

    first commit

---
+++ src/main/java/com/cg/keyvault/azkeyvault/AzkeyvaultApplication.java
@@ -0,0 +1,28 @@
+package com.cg.keyvault.azkeyvault;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzkeyvaultApplication implements CommandLineRunner {
+	
+	@Value("${sampleProperty1}")
+    private String sampleProperty1;
+    @Value("${sampleProperty2}")
+    private String sampleProperty2;
+    @Value("${samplePropertyInMultipleKeyVault}")
+    private String samplePropertyInMultipleKeyVault;
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzkeyvaultApplication.class, args);
+	}
+	
+	public void run(String[] args) {
+        System.out.println("sampleProperty1: " + sampleProperty1);
+        System.out.println("sampleProperty2: " + sampleProperty2);
+        System.out.println("samplePropertyInMultipleKeyVault: " + samplePropertyInMultipleKeyVault);
+    }
+
+}
---
+++ src/test/java/com/cg/keyvault/azkeyvault/AzkeyvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.keyvault.azkeyvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzkeyvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
